f4976808bf2d61a94c16c32dfe65898f5e6ec66c
Bao Tran The <tran62the61ba03@gmail.com>
2023-11-07 20:53:19 +0700

    version 2.0

--- prime.cpp
+++ prime.cpp
@@ -3,7 +3,13 @@ bool checkPrime(int N) {
     if (N <= 1) {
         return 0;
     }
-    for (int i = N - 1; i > 1; --i) {
+    if (N == 2) {
+        return 1;
+    }
+    if (N % 2 == 0) {
+        return 0;
+    }
+    for (int i = 3; i * i <= N; i += 2) {
         if (N % i == 0) {
             return 0;
         }
